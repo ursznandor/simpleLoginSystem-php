75290171f8ed11a5fd3e3a1eb1811eb43a6e7fc5
Nándor Urszuly <ursznandor@users.noreply.github.com>
2017-05-21 19:52:56 +0200

    input name changes

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 </head>
 <body>
 	<form action="signup.php" method="POST">
-		<input type="text" name="first" placeholder="Firstname"><br>
-		<input type="text" name="last" placeholder="Lastname"><br>
+		<input type="text" name="firstname" placeholder="Firstname"><br>
+		<input type="text" name="lastname" placeholder="Lastname"><br>
 		<input type="text" name="nick" placeholder="Username"><br>
 		<input type="password" name="pswd" placeholder="Password"><br>
 		<button type="submit" name="submit" value="submit">Sign In</button>

a2b7cb991e5118489f05ad2457bb80d69fcb7967
Nándor Urszuly <ursznandor@users.noreply.github.com>
2017-05-21 18:55:16 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Simple login system in php</title>
+</head>
+<body>
+	<form>
+		<input type="text" name="first" placeholder="Firstname">
+		<input type="text" name="last" placeholder="Lastname">
+		<input type="text" name="nick" placeholder="Username">
+		<input type="password" name="pswd" placeholder="Password">
+		<button type="submit" name="submit" value="submit">Sign In</button>
+	</form>
+</body>
+</html>
